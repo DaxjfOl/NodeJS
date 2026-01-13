9741e416e4ca453e66b90d68044cb47929ff5b0b
DaxjfOl <vietha14122k1@gmail.com>
2026-01-13 19:55:47 +0700

    Ngay 4

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,6 @@
 const { render } = require('ejs');
 const connection = require('../config/database');
+const e = require('express');
 
 
 
@@ -14,8 +15,26 @@ const getDaxjfPage = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    console.log(req.body);
-    res.send('Create a new user')
+    // console.log(req.body);
+    let email = req.body.email;
+    let name = req.body.name;
+    let city = req.body.city;
+
+    console.log(email, name, city);
+
+
+
+    connection.query(
+        ` INSERT INTO Users (email, name, city) 
+          VALUES (?, ?, ?);`,
+        [email, name, city],
+        function (err, results) {
+            console.log(results);
+
+            res.send('Post create user successfully!')
+        }
+    );
+
 }
 
 module.exports = {
